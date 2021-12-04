9d299bc43b7222452470092f1c3dd58e4a18750a
Aditya Prakash <94276846+adityaprakash30@users.noreply.github.com>
2021-12-04 17:45:30 +0530

    functions

---
+++ function.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int timesTwo(int x) {
+   return x*2;
+}
+int main(){
+    cout << timesTwo(8) << endl;
+    cout << timesTwo(16) << endl;
+    cout << timesTwo(54) << endl;
+}
