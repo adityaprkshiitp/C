8edc33779f36007dc9f0a67ca2b965fdd7d46c06
Aditya Prakash <prakashaditya25957@gmail.com>
2022-04-21 12:24:23 +0530

    new

--- cal.cpp
+++ cal.cpp
@@ -9,10 +9,10 @@ int main()
     sub=b-a;
     mult=a*b;
     div=b/a;
-    cout<<"Addition of A and B is"<<add<<endl;
-    cout<<"Subtraction of A and B is"<<sub<<endl;
-    cout<<"Multiplication of A and B is"<<mult<<endl;
-    cout<<"Division of A and B is"<<div<<endl;
+    cout<<"Addition of A and B is "<<add<<endl;
+    cout<<"Subtraction of A and B is "<<sub<<endl;
+    cout<<"Multiplication of A and B is "<<mult<<endl;
+    cout<<"Division of A and B is "<<div<<endl;
 
 
 } 
